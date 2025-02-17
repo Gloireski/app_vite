5748be6f961a704e8a527d64403b1cc8b1bd94b3
Gloire Bekoutou Belem <belem@Gloires-MacBook-Air.local>
2025-02-17 15:54:09 +0100

    task 2

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -7,7 +7,8 @@ type Task = {
 const Task: React.FC<Task> = ({ id, title, isDone }) => {
     return (
         <div className="flex flex-col items-center justify-center
-        bg-blue-200/10 p-4 m-4 rounded-lg shadow-lg">
+            bg-blue-200/10 p-4 m-4 rounded-lg shadow-lg
+            w-96 hover:shadow-xl transition duration-500">
         <h2 className="text-2xl text-amber-900">{title}</h2>
         <p className="text-xl text-blue-300">{isDone}</p>
         </div>
